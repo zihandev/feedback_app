7c1543e833315aaf81a5b1ed704f0e6c7de53c4c
zihan <code.everyday7@gmail.com>
2019-07-06 19:07:59 +0530

    just

--- services/surveyTemplate.js
+++ services/surveyTemplate.js
@@ -1,4 +1,19 @@
-module.exports = survey =>{
-
-    return `HTML ${survey.body}`
-}
+module.exports = survey => {
+    return `
+      <html>
+        <body>
+          <div style="text-align: center;">
+            <h3>I'd like your input!</h3>
+            <p>Please answer the following question:</p>
+            <p>${survey.body}</p>
+            <div>
+              <a href="https://shielded-plateau-50141.herokuapp.com/api/surveys/${survey.id}/yes">Yes</a>
+            </div>
+            <div>
+              <a href="https://shielded-plateau-50141.herokuapp.com/api/surveys/${survey.id}/no">No</a>
+            </div>
+          </div>
+        </body>
+      </html>
+    `;
+  };
