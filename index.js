18659e1d3275023a27b778d881bb448191832a2f
zihan <code.everyday7@gmail.com>
2019-07-06 16:49:16 +0530

    again

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ require('./routes/authRoute')(app);
 require('./routes/billingRoute')(app);
 require('./routes/surveyRoute')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
-const PORT = 5000
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log('HI'))
